3a2241e0d4ddb7529dc1887497b99005e0d294bd
MZiegler <mwziegler2014@gmail.com>
2017-11-19 10:50:27 -0500

    verified that comments still work after some cleanup

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,9 +1,8 @@
-	
-var commentNbr =0;
+var commentNbr = 0;
 var addCommentButton = document.getElementById('addCommentButton');
 
 function deleteComment(number) {
-	
+
 	var idNameToDelete = 'commentDiv' + number;
 	var elementToDelete = document.getElementById(idNameToDelete);
 	alert(idNameToDelete);
@@ -11,54 +10,38 @@ function deleteComment(number) {
 }
 
 addCommentButton.addEventListener('click', function() {
-		commentNbr = commentNbr + 1;
-		var userResponse = prompt('Enter your comment:');
-	    
-	    var para = document.createElement("p");
-	    para.className = 'commentNbr' + commentNbr;
-	    var node = document.createTextNode(userResponse);
-	    para.appendChild(node);
-	  
-	    var delButton = document.createElement("button");
-	    delButton.className = 'commentNbr' + commentNbr;
-	  
-	    delButton.addEventListener('click', function() {
-	    	deleteComment(commentNbr);
-	    });
-	    
-	    var node2 = document.createTextNode("delete this comment");
-	    delButton.appendChild(node2);
-	    
-	    var element = document.getElementById("comments");
-	    //element.appendChild(delButton);
-	    //element.appendChild(para);
-	    
-	    /* test stuff here */
-		var div = document.createElement("div");
-		var specificClass='commentNbr' + commentNbr;
-		div.classList.add(specificClass, "aComment");
-		div.id = 'commentDiv' + commentNbr;
-		//div.className = 'aComment';
-		element.appendChild(div);
-		div.appendChild(delButton);
-		div.appendChild(para);
-	    /*
-		var iDiv = document.createElement('div');
-		iDiv.id = 'block';
-		iDiv.className = 'block';
-		document.getElementsByTagName('body')[0].appendChild(iDiv);
+	commentNbr = commentNbr + 1;
+	var userResponse = prompt('Enter your comment:');
 
-		// Now create and append to iDiv
-		var innerDiv = document.createElement('div');
-		innerDiv.className = 'block-2';
-		*/
-		
-		
-		   
+	var para = document.createElement("p");
+	para.className = 'commentNbr' + commentNbr;
+	var node = document.createTextNode(userResponse);
+	para.appendChild(node);
+
+	var delButton = document.createElement("button");
+	delButton.className = 'commentNbr' + commentNbr;
+
+	delButton.addEventListener('click', function() {
+		deleteComment(commentNbr);
+	});
+
+	var node2 = document.createTextNode("delete this comment");
+	delButton.appendChild(node2);
+
+	var element = document.getElementById("comments");
+	
+	var div = document.createElement("div");
+	var specificClass = 'commentNbr' + commentNbr;
+	div.classList.add(specificClass, "aComment");
+	div.id = 'commentDiv' + commentNbr;
+	element.appendChild(div);
+	div.appendChild(delButton);
+	div.appendChild(para);
+
 });
 
-    /*
-    element.addEventListener('mouseout', function() {
-		this.style[cssProp] = value2;
-	});
-    */
+/*
+element.addEventListener('mouseout', function() {
+	this.style[cssProp] = value2;
+});
+ */
