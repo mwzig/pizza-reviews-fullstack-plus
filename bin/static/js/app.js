8c653e6de01691abf6fd9595e01219b1da6018b3
MZiegler <mwziegler2014@gmail.com>
2017-11-18 19:40:24 -0500

    the add and delete comment logic works

--- bin/static/js/app.js
+++ bin/static/js/app.js
@@ -2,27 +2,43 @@
 var commentNbr =0;
 var addCommentButton = document.getElementById('addCommentButton');
 
+function deleteComment(number) {
+	
+	var idNameToDelete = 'commentDiv' + number;
+	var elementToDelete = document.getElementById(idNameToDelete);
+	alert(idNameToDelete);
+	elementToDelete.parentNode.removeChild(elementToDelete);
+}
 
 addCommentButton.addEventListener('click', function() {
 		commentNbr = commentNbr + 1;
-	  var userResponse = prompt('Enter your comment:');
+		var userResponse = prompt('Enter your comment:');
 	    
 	    var para = document.createElement("p");
+	    para.className = 'commentNbr' + commentNbr;
 	    var node = document.createTextNode(userResponse);
 	    para.appendChild(node);
 	  
 	    var delButton = document.createElement("button");
+	    delButton.className = 'commentNbr' + commentNbr;
+	  
+	    delButton.addEventListener('click', function() {
+	    	deleteComment(commentNbr);
+	    });
+	    
 	    var node2 = document.createTextNode("delete this comment");
 	    delButton.appendChild(node2);
 	    
 	    var element = document.getElementById("comments");
-	    element.appendChild(delButton);
-	    element.appendChild(para);
+	    //element.appendChild(delButton);
+	    //element.appendChild(para);
 	    
 	    /* test stuff here */
 		var div = document.createElement("div");
-		div.id='comment' + commentNbr;
-		div.className='newComment';
+		var specificClass='commentNbr' + commentNbr;
+		div.classList.add(specificClass, "aComment");
+		div.id = 'commentDiv' + commentNbr;
+		//div.className = 'aComment';
 		element.appendChild(div);
 		div.appendChild(delButton);
 		div.appendChild(para);
